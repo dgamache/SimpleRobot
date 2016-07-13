0716c83031524e8b3057730c27f69b48f6021007
David Gamache <dag@dagnetsolutions.com>
2016-07-12 21:31:30 -0400

    Update with turn signal
    Update to flash LED when turning.

--- SimpleRobot.ino
+++ SimpleRobot.ino
@@ -2,11 +2,38 @@
 const int motor_l = 2;
 const int motor_r = 4;
 const int bumper = 12;
+const int left_directional = 7;
+const int right_directional = 8;
 
 const int stop = 0;
 const int forward = 1;
 const int left = 2;
 const int right = 3;
+
+void directional (int direction)
+{
+  switch (direction) {
+    case left:
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(left_directional, HIGH);
+        delay (1000);
+        digitalWrite(left_directional, LOW);
+        delay (1000);
+      }
+      break;
+    case right:
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(right_directional, HIGH);
+        delay (500);
+        digitalWrite(right_directional, LOW);
+        delay (500);
+      }
+      break;
+    default:
+      Serial.println("not allowed");
+      return;
+  }
+}
 
 // Command the motors
 void go (int direction)
@@ -27,14 +54,14 @@ void go (int direction)
       digitalWrite(motor_l, LOW);
       digitalWrite(motor_r, HIGH);
       // Should turn in half a second
-      delay (500);
+      directional(left);
       break;
     case right:
       Serial.println("right");
       digitalWrite(motor_l, HIGH);
       digitalWrite(motor_r, LOW);
       // Should turn in half a second
-      delay (500);
+      directional(left);
       break;
     default:
       Serial.println("not allowed");
@@ -46,10 +73,10 @@ void go (int direction)
 bool obstacle_detected()
 {
   int result = digitalRead(bumper);
-  if (result == HIGH){
+  if (result == HIGH) {
     return (true);
   }
-  
+
   return (false);
 }
 
@@ -58,6 +85,8 @@ void setup() {
   pinMode(motor_l, OUTPUT);
   pinMode(motor_r, OUTPUT);
   pinMode(bumper, INPUT);
+  pinMode(left_directional, OUTPUT);
+  pinMode(right_directional, OUTPUT);
   Serial.begin(9600);
 }
 
